ef34413aebdfe53cdc2f3214469ec39d3a9f3646
harrysudanaa <komangharry02@gmail.com>
2024-09-17 16:12:49 +0800

    add faq section

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 // pages/dashboardPage.js
 import About from "@/components/dashboard/About";
+import FAQ  from "@/components/dashboard/FAQ";
 import HeroSection from "@/components/dashboard/HeroSection";
 import Service from "@/components/dashboard/Service";
 import React from "react";
@@ -10,6 +11,7 @@ const DashboardPage = () => {
       <HeroSection />
       <About/>
       <Service />
+      <FAQ/>
     </div>
   );
 };
